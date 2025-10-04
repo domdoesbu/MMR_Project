cd577435514359289d873f2f486f6585a63a7ae0
leon-crt <leon.pirazzoli00@gmail.com>
2025-10-04 14:32:06 +0200

    working on rectangularity

--- MMR_Project/src/ElementaryFeatures.cpp
+++ MMR_Project/src/ElementaryFeatures.cpp
@@ -7,16 +7,8 @@ float ElementaryFeatures::SurfaceArea(std::vector<float> positions)
         glm::vec3 v1(positions[i + 0], positions[i + 1], positions[i + 2]);
         glm::vec3 v2(positions[i + 6], positions[i + 7], positions[i + 8]);
         glm::vec3 v3(positions[i + 12], positions[i + 13], positions[i + 14]);
-        glm::vec3 localBarycenter(0.0f);
 
-        glm::vec3 e1 = v2 - v1;
-        glm::vec3 e2 = v3 - v1;
-        glm::vec3 cross = glm::cross(e1, e2);
-
-        float localArea = glm::length(cross) * 0.5f; // triangle area
-
-        // sum (x * area)
-        // / sum area
+        float localArea = ComputeLocalArea(v1, v2, v3); // triangle area
 
         sumOfAreas += localArea;
     }
@@ -24,8 +16,10 @@ float ElementaryFeatures::SurfaceArea(std::vector<float> positions)
 }
 
 
-float ElementaryFeatures::Rectangularity(std::vector<float> positions)
+float ElementaryFeatures::Rectangularity(std::vector<float> positions, float barycenter[3])
 {
+    // 1. Get eigen vector aligned bounding volume (which after full normalization is just the axis aligned one) 
+    // 2. Compute the ratio between the mesh volume and that of the bounding volume
 
 }
 
@@ -33,4 +27,27 @@ float ElementaryFeatures::Rectangularity(std::vector<float> positions)
 float ElementaryFeatures::Convexity(std::vector<float> positions)
 {
 
+}
+
+float ElementaryFeatures::ComputeVolume(std::vector<float> positions, std::string faceType, float barycenter[3])
+{
+    // Iterate through all faces and compute volume of the tetrahedron with the face as base and the barycenter as the tip
+    for (int i = 0; i + 14 < positions.size(); i += 18) {
+        glm::vec3 v1(positions[i + 0], positions[i + 1], positions[i + 2]);
+        glm::vec3 v2(positions[i + 6], positions[i + 7], positions[i + 8]);
+        glm::vec3 v3(positions[i + 12], positions[i + 13], positions[i + 14]);
+
+        float baseArea = ComputeLocalArea(v1, v2, v3);
+
+        
+    }
+}
+
+float ElementaryFeatures::ComputeLocalArea(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3)
+{
+    glm::vec3 e1 = v2 - v1;
+    glm::vec3 e2 = v3 - v1;
+    glm::vec3 cross = glm::cross(e1, e2);
+
+    return glm::length(cross) * 0.5f; // triangle area
 }
--- MMR_Project/src/ElementaryFeatures.h
+++ MMR_Project/src/ElementaryFeatures.h
@@ -5,15 +5,20 @@
 
 class ElementaryFeatures
 {
+public:
 	ElementaryFeatures() {};
 
 	float SurfaceArea(std::vector<float> positions);
 
-	// shape volume divided by OBB volume, should return a histogram
-	float Rectangularity(std::vector<float> positions);
+	// shape volume divided by OBB volume
+	float Rectangularity(std::vector<float> positions, float barycenter[3]);
 
-	// shape volume divided by convex hull volume, should return a histogram
+	// shape volume divided by convex hull volume, should return a histogram?
 	float Convexity(std::vector<float> positions);
 
+private:
+	float ComputeVolume(std::vector<float> positions, std::string faceType, float barycenter[3]);
+
+	float ComputeLocalArea(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3);
 };
 
