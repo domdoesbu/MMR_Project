75b585432c77779d3e85d3c0936f234ca7da3079
leon-crt <leon.pirazzoli00@gmail.com>
2025-10-05 10:30:46 +0200

    volume computation function done (not tested tho)

--- MMR_Project/src/ElementaryFeatures.cpp
+++ MMR_Project/src/ElementaryFeatures.cpp
@@ -31,6 +31,7 @@ float ElementaryFeatures::Convexity(std::vector<float> positions)
 
 float ElementaryFeatures::ComputeVolume(std::vector<float> positions, std::string faceType, float barycenter[3])
 {
+    float fullVolume = 0.0;
     // Iterate through all faces and compute volume of the tetrahedron with the face as base and the barycenter as the tip
     for (int i = 0; i + 14 < positions.size(); i += 18) {
         glm::vec3 v1(positions[i + 0], positions[i + 1], positions[i + 2]);
@@ -39,8 +40,19 @@ float ElementaryFeatures::ComputeVolume(std::vector<float> positions, std::strin
 
         float baseArea = ComputeLocalArea(v1, v2, v3);
 
-        
+        // Volume = 1/3 * B * h
+        // Find the local barycenter of the face
+        glm::vec3 localBarycenter = v1 + v2 + v3;
+        localBarycenter.x = localBarycenter.x / 3;
+        localBarycenter.y = localBarycenter.y / 3;
+        localBarycenter.z = localBarycenter.z / 3;
+
+        // We need to find the distance from the face barycenter to the mesh barycenter (that's h)
+        float height = sqrt(pow(barycenter[0] - localBarycenter[0], 2) + pow(barycenter[1] - localBarycenter[1], 2) + pow(barycenter[2] - localBarycenter[2], 2));
+        float localVolume = (1 / 3) * baseArea * height;
+        fullVolume += localVolume;
     }
+    return fullVolume;
 }
 
 float ElementaryFeatures::ComputeLocalArea(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3)
