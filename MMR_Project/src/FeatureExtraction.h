ebae19d248d97f0462a9bd365235db094ea1501b
Dom Doesburg <94422248+domdoesbu@users.noreply.github.com>
2025-10-10 11:57:05 +0200

    Fixed A3-D4

--- MMR_Project/src/FeatureExtraction.h
+++ MMR_Project/src/FeatureExtraction.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include<glm/glm.hpp>
 #include"FileOrganizer.h"
+#include "matplotlibcpp.h"
 #include<MRMesh/MRConvexHull.h>
 
 class FeatureExtraction
@@ -10,45 +11,38 @@ class FeatureExtraction
 public:
 	FeatureExtraction() {};
 
-
-
+	// Surface area S
 	float SurfaceArea(std::vector<float> positions);
 
 	// shape volume divided by OBB volume
+	// // 3D rectangularity
 	float Rectangularity(std::vector<float> positions, float barycenter[3], std::string shapeFilename, std::string csvFilename);
-
+	
+	// Convexity (shape volume / convex hull volume)
 	// shape volume divided by convex hull volume, should return a histogram?
 	float Convexity(std::vector<float> positions, float barycenter[3]);
-
-
-	// Volume V
-
-	// Surface area S
 
 	// Compactness
 	// S^3/(36*Pi*V^2)
 
-
-	// 3D rectangularity
-
 	// Diameter: largest distance between any two surface points
 	float Diameter(std::vector<float>& positions);
-	// Convexity (shape volume / convex hull volume)
-
+	
 	// Eccentricity (ratio of largest to smallest eigenvalues of covariance matrix)
 	float Eccentricity(float largeEig, float smallEig);
 	// A3
-	float A3(std::vector<float>& positions);
+	void A3(std::vector<float>& positions, int samples, int bins);
 	// D1
-	float D1(std::vector<float>& positions, glm::vec3 barycenter);
+	void D1(std::vector<float>& positions, glm::vec3 barycenter, int samples, int bins);
 	// D2
-	float D2(std::vector<float>& positions);
+	void D2(std::vector<float>& positions, int samples, int bins);
 	// D3
-	float D3(std::vector<float>& positions);
+	void D3(std::vector<float>& positions, int samples, int bins);
 	// D4
-	float D4(std::vector<float>& positions);
+	void D4(std::vector<float>& positions, int samples, int bins);
 
 private:
+	// Volume V
 	float ComputeVolume(std::vector<float> positions, std::string faceType, float barycenter[3]);
 
 	float ComputeLocalArea(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3);
