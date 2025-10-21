f79ce0fce8b1b1b49dd92c51de98cf6fccf53ff6
leon-crt <leon.pirazzoli00@gmail.com>
2025-10-21 10:37:33 +0200

    kinda fixed the normalization function still not good

--- MMR_Project/src/Querying.cpp
+++ MMR_Project/src/Querying.cpp
@@ -21,6 +21,9 @@ void Querying::Normalization(std::string databasePath, std::string csvFilename)
     float avgEccentricity = 0.0;
     int totalCount = 0;
 
+    //read first line of column names
+    std::getline(csvFile, line);
+
     // reading feature values and accumulating to calculate averages
     while (std::getline(csvFile, line)) {
         std::istringstream iss(line);
@@ -28,40 +31,49 @@ void Querying::Normalization(std::string databasePath, std::string csvFilename)
         ShapeFeatures currentFeatures;
         std::getline(iss, token, ',');
         std::getline(iss, token, ',');
-        std::getline(iss, token, ','); currentFeatures.surfaceArea = std::stof(token); avgArea += std::stof(token);
+        std::getline(iss, token, ','); 
+        currentFeatures.surfaceArea = std::stof(token); 
+        avgArea += std::stof(token);
         std::getline(iss, token, ','); currentFeatures.volume = std::stof(token); avgVolume += std::stof(token);
         std::getline(iss, token, ','); currentFeatures.compactness = std::stof(token); avgCompactness += std::stof(token);
         std::getline(iss, token, ','); currentFeatures.rectangularity = std::stof(token); avgRectangularity += std::stof(token);
         std::getline(iss, token, ','); currentFeatures.diameter = std::stof(token); avgDiameter += std::stof(token);
         std::getline(iss, token, ','); currentFeatures.convexity = std::stof(token); avgConvexity += std::stof(token);
         std::getline(iss, token, ','); currentFeatures.eccentricity = std::stof(token); avgEccentricity += std::stof(token);
-        std::vector<double> a3;
-        std::vector<double> d1;
-        std::vector<double> d2;
-        std::vector<double> d3;
-        std::vector<double> d4;
+        
+        int totalBinSize = 20 + 30 + 20 + 20 + 20;
+        std::vector<double> a3(totalBinSize);
+        std::vector<double> d1(totalBinSize);
+        std::vector<double> d2(totalBinSize);
+        std::vector<double> d3(totalBinSize);
+        std::vector<double> d4(totalBinSize);
 
-        for (int i = 0; i < (20 + 30 + 20 + 20 + 20); ++i)
+        for (int i = 0; i < totalBinSize; i++)
         {
             if (i < 20)
             {
-                std::getline(iss, token, ','); a3[i] = std::stof(token);
+                std::getline(iss, token, ','); 
+                a3[i] = std::stof(token);
             }
             else if (i < (20 + 30))
             {
-                std::getline(iss, token, ','); d1[i] = std::stof(token);
+                std::getline(iss, token, ','); 
+                d1[i] = std::stof(token);
             }
             else if (i < (20 + 30 + 20))
             {
-                std::getline(iss, token, ','); d2[i] = std::stof(token);
+                std::getline(iss, token, ','); 
+                d2[i] = std::stof(token);
             }
             else if (i < (20 + 30 + 20 + 20))
             {
-                std::getline(iss, token, ','); d3[i] = std::stof(token);
+                std::getline(iss, token, ',');
+                d3[i] = std::stof(token);
             }
             else
             {
-                std::getline(iss, token, ','); d4[i] = std::stof(token);
+                std::getline(iss, token, ','); 
+                d4[i] = std::stof(token);
             }
         }
         currentFeatures.A3 = a3;
@@ -70,6 +82,7 @@ void Querying::Normalization(std::string databasePath, std::string csvFilename)
         currentFeatures.D3 = d3;
         currentFeatures.D4 = d4;
         totalFeatures.push_back(currentFeatures);
+        ++totalCount;
     }
     csvFile.close();
 
@@ -96,6 +109,7 @@ void Querying::Normalization(std::string databasePath, std::string csvFilename)
         stdDevArea += pow(totalFeatures[i].surfaceArea - avgArea, 2.0);
         stdDevVolume += pow(totalFeatures[i].volume - avgVolume, 2.0);
         stdDevCompactness += pow(totalFeatures[i].compactness - avgCompactness, 2.0);
+        stdDevRectangularity += pow(totalFeatures[i].rectangularity - avgRectangularity, 2.0);
         stdDevDiameter += pow(totalFeatures[i].diameter - avgDiameter, 2.0);
         stdDevConvexity += pow(totalFeatures[i].convexity - avgConvexity, 2.0);
         stdDevEccentricity += pow(totalFeatures[i].eccentricity - avgEccentricity, 2.0);
@@ -111,7 +125,7 @@ void Querying::Normalization(std::string databasePath, std::string csvFilename)
 
     // actually normalizing feature values
 
-    std::vector<float> normSurfaceArea, normVolume, normCompactness, normRectangularity, normDiameter, normConvexity, normEccentricity;
+    std::vector<float> normSurfaceArea(totalFeatures.size()), normVolume(totalFeatures.size()), normCompactness(totalFeatures.size()), normRectangularity(totalFeatures.size()), normDiameter(totalFeatures.size()), normConvexity(totalFeatures.size()), normEccentricity(totalFeatures.size());
     std::vector<std::vector<double>> a3;
     std::vector<std::vector<double>> d1;
     std::vector<std::vector<double>> d2;
@@ -120,13 +134,13 @@ void Querying::Normalization(std::string databasePath, std::string csvFilename)
     for (int i = 0; i < totalFeatures.size(); ++i)
     {
         normSurfaceArea[i] = (totalFeatures[i].surfaceArea - avgArea) / stdDevArea;
-        normVolume[i]= (totalFeatures[i].volume- avgVolume) / stdDevVolume;
-        normCompactness[i] = (totalFeatures[i].compactness- avgCompactness) / stdDevCompactness;
-        normRectangularity[i] = (totalFeatures[i].rectangularity- avgRectangularity) / stdDevRectangularity;
-        normDiameter[i] = (totalFeatures[i].diameter- avgDiameter) / stdDevDiameter;
+        normVolume[i]= (totalFeatures[i].volume - avgVolume) / stdDevVolume;
+        normCompactness[i] = (totalFeatures[i].compactness - avgCompactness) / stdDevCompactness;
+        normRectangularity[i] = (totalFeatures[i].rectangularity - avgRectangularity) / stdDevRectangularity;
+        normDiameter[i] = (totalFeatures[i].diameter - avgDiameter) / stdDevDiameter;
         normConvexity[i] = (totalFeatures[i].convexity - avgConvexity) / stdDevConvexity;
         normEccentricity[i] = (totalFeatures[i].eccentricity - avgEccentricity) / stdDevEccentricity;
-        a3.push_back(totalFeatures[i].A3); // Ayo Dom do you think this are gonna be in the right order when we put them in the csv?
+        a3.push_back(totalFeatures[i].A3);
         d1.push_back(totalFeatures[i].D1);
         d2.push_back(totalFeatures[i].D2);
         d3.push_back(totalFeatures[i].D3);
@@ -135,7 +149,7 @@ void Querying::Normalization(std::string databasePath, std::string csvFilename)
 
     FileOrganizer fo;
     
-    fo.WriteCSVFeatureExtraction(databasePath, "feature_extraction_complete.csv",
+    fo.WriteCSVFeatureExtraction(databasePath, "feature_extraction_complete_normalized.csv",
         normSurfaceArea,
         normVolume,
         normCompactness,
